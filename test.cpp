12ab037e6d3d7a7ba9bf5ea3bf56b3f3ed1494bf
shuangde <zengshuangde@gmail.com>
2013-08-18 14:27:52 +0800

    test

---
+++ test.cpp
@@ -0,0 +1,33 @@
+/**==========================================
+ *   This is a solution for ACM/ICPC problem
+ *   
+ *   @source: HDU 4649 Professor Tian
+ *   @type:  dp
+ *   @author: shuangde
+ *   @blog: blog.csdn.net/shuangde800
+ *   @email: zengshuangde@gmail.com
+ *===========================================*/
+
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+#include<vector>
+#include<map>
+using namespace std;
+
+typedef long long int64;
+const int INF = 0x3f3f3f3f;
+const int MAXN = 210;
+
+int n, m;
+double f[MAXN][2];
+int A[MAXN];
+char O[MAXN];
+double p[MAXN];
+
+int main(){
+
+    printf("Hello world!!\n");
+    return 0;
+}
