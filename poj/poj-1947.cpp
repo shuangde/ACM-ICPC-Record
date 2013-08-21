fc36970e09666a9ed11b1cc8292cfca334aee180
shuangde <zengshuangde@gmail.com>
2013-08-21 15:51:54 +0800

    Accepted and Described

--- poj/poj-1947.cpp
+++ poj/poj-1947.cpp
@@ -9,6 +9,27 @@
  *   Copyright (C) 2013/08/19 14:30 All rights reserved. 
  *======================================================*/
 
+/*
+题意:
+    给一棵树,问最少删掉几条边.使得剩下的子树中有节点个数为p个的
+
+思路:
+    之前不知道看到谁说这题水, 但是为什么我就不觉得水呢? 3天前就看了这题,
+    但是没什么想法,然后每天都有去想一下, 一直到今天, 在我对自己方法还有怀疑
+    的情况下,竟然AC了..
+
+    f(i, j) 表示子树i,保留j个节点的最少删边次数, 注意,这里保留的j个节点的子树,是指根节点
+    为i的j个节点的子树,这样理解的话, 状态转移就容易想多了.
+
+    对于子树i, 如果只保留1个节点,那么连接它所有儿子节点的边都要删掉,
+    所以可以初始化 f(i, 1) = 节点i的儿子个数
+
+    f(i, j) = max{ max{f(i,j-k) + f(v, k) - 1 | 1<=k<s} | v是i的儿子节点 }
+
+    最终ans = min{ f(i, p) }
+
+*/
+
 #include <iostream>
 #include <cstdio>
 #include <algorithm>
@@ -22,64 +43,53 @@ using namespace std;
 typedef pair<int, int >PII;
 typedef long long int64;
 const int INF = 0x3f3f3f3f;
-const double PI  = acos(-1.0);
 
 const int MAXN = 160;
 vector<int>adj[MAXN];
 int n, p;
 int f[MAXN][MAXN], tot[MAXN];
-bool vis[MAXN];
+int ans;
 
-// 计算子树节点
-int dfs1(int u) {
-    vis[u] = true;
-    int& sum = tot[u] = 1;
+int dp(int u) {
+
+    // 计算子树有几个节点
+    tot[u] = 1;
     for (int i = 0; i < adj[u].size(); ++i) {
         int v = adj[u][i];
-        if (vis[v]) continue;
-        sum += dfs1(v);
+        tot[u] += dp(v); 
     }
-    return sum;
-}
 
-void dp(int u) {
-    vis[u] = true;
-    f[u][tot[u]] = 0;
+    // init
+    f[u][1] = adj[u].size();
+    // dp
     for (int i = 0; i < adj[u].size(); ++i) {
         int v = adj[u][i];
-        if (vis[v]) continue;
-        dp(v);
-        f[u][tot[v]] = f[u][tot[u]-tot[v]] = 1;
-        for (int s = 1; s <= tot[v]; ++s) {
-            f[u][s] = min(f[u][s], f[v][s] + 1);      
+        for (int s = tot[u]; s >= 1; --s) {
+            for (int j = 1; j < s && j <= tot[v]; ++j)
+                f[u][s] = min(f[u][s], f[u][s-j] + f[v][j] - 1);
         }
     }
-    printf("%d:\n", u);
-    for (int s = 1; s <= tot[u]; ++s) {
-        printf("%d ", f[u][s]);
-    }
-    puts("\n==============");
+    if (tot[u] >= p)
+        ans = min(ans, f[u][p] + (u!=1));
+    return tot[u];
 }
-
 
 int main(){
 
+    for (int i = 0; i <=n; ++i)
+        adj[i].clear();
+
     scanf("%d %d", &n, &p);
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n - 1; ++i) {
         int u, v;
         scanf("%d%d", &u, &v);
         adj[u].push_back(v);
-        adj[v].push_back(u);
     }
-    memset(vis, 0, sizeof(vis));
-    dfs1(1);
 
     memset(f, INF, sizeof(f));
-    memset(vis, 0, sizeof(vis));
+    ans = INF;
     dp(1);
-    
-    for (int i = 1; i <= p; ++i)
-        printf("%d ", f[1][i]);
-    printf("%d\n", f[1][p]);
+
+    printf("%d\n", ans);
     return 0;
 }
