8b54ffc2f861fec16390a3b8ec2fb6dc9b76e80f
shuangde <zengshuangde@gmail.com>
2013-11-21 13:21:08 +0800

    update

--- test.cpp
+++ test.cpp
@@ -1,127 +1,79 @@
-#include <cmath>
-#include <cstdio>
-#include <cstring>
-#include <string>
-#include <queue>
-#include <functional>
-#include <set>
 #include <iostream>
-#include <vector>
+#include <cstdio>
 #include <algorithm>
+#include <cstring>
+#include <cstdlib>
+// 0~x-1
+#define random(x) (rand()%x)
 using namespace std;
 
+const int N = 1e9;
 typedef long long int64;
-const int MAXN = 100010;
-int n; int64 v;
 
-struct Node {
-    int t, p, id;
-    bool operator<(const Node&rhs)const{
-        return p > rhs.p;
-    }
-};
-vector<Node>A, B;
-vector<int>ans;
-int pa, pb, lenA, lenB;
-int64 maxx = 0, vo = 0;
 
-int64 solve() {
+int64 a, b, c, d, p, m;
+int64 one, two, three, four; 
 
-    maxx = 0, vo = 0;
-    while (vo + 2 <= v) {
-        int tmp = A[pa].p + A[pa+1].p;
-        if (tmp > B[pb].p) {
-            maxx += tmp;
-            ans.push_back(A[pa].id);
-            if (A[pa+1].id != -1) ans.push_back(A[pa+1].id);
-            vo += 2;
-            pa += 2;
-        } else {
-            maxx += B[pb].p;
-            ans.push_back(B[pb].id);
-            vo += 2;
-            pb++;
-        }
-    }
-
-    if (v-vo && pa < lenA) {
-        maxx += A[pa].p;
-        ans.push_back(A[pa].id);
-    }
-
+int64 gcd(long long a,long long b){
+        while(b^=a^=b^=a%=b);
+            return a;
 }
 
-int main () {
+int64 countDown(int64 x, int64 y) {
+    int64 tmp = x - m + p;
+    int64 start = tmp%p==0 ? x : x+(p-tmp%p);
+    tmp = y - m + p;
+    int64 end   = tmp%p==0 ? y : y-(tmp%p);
+    if (start > end) return 0;
+    start = start-a-c+1; end = end-a-c+1;
+    return (start+end)*((end-start)/p+1)/2;
+}
 
-    cin >> n >> v;
+int64 countUp(int64 x, int64 y) {
+    int64 tmp = x - m + p;
+    int64 start = tmp%p==0 ? x : x+(p-tmp%p);
+    tmp = y - m + p;
+    int64 end   = tmp%p==0 ? y : y-(tmp%p);
+    if (start > end) return 0;
+    start -= b;  end -= b;
+    start = d - start + 1; end = d - end + 1;
+    return (start+end)*((start-end)/p+1)/2;
+} 
 
-    int64 sumV = 0, sumP = 0;
-    for (int i = 0; i < n; ++i) {
-        int t, p;
-        scanf("%d%d", &t, &p);
-        sumV += t;
-        sumP += p;
-        if (t == 1) {
-            A.push_back((Node){t,p,i+1});
-        } else {
-            B.push_back((Node){t,p,i+1});
+int64 countMid(int64 x, int64 y) { 
+    int64 tmp = x - m + p;
+    int64 start = tmp%p==0 ? x : (x+(p-tmp%p));
+    tmp = y - m + p;
+    int64 end   = tmp%p==0 ? y : y-(tmp%p);
+    if (start > end) return 0;
+    return ((end-start)/p+1) * (d-c+1);
+}
+
+int main(){
+
+    cout << "Hello world!" << endl;
+    return 0;
+
+    int T;
+    scanf("%d", &T);
+    for (int cas = 1; cas <= T; ++cas) {
+
+        cin >> a >> b >> c >> d >> p >> m;
+
+        if (b-a < d-c) {
+            swap(a, c); swap(b, d);
         }
-    }
-
-    if (v >= sumV) {
-        cout << sumP << endl;
-        for (int i = 1; i <= n; ++i)
-            printf("%d ", i);
-        puts("");
-        return 0;
-    }
-
-    sort(A.begin(), A.end());
-    sort(B.begin(), B.end());
-
-    pa = pb = 0;
-    int lenA = A.size(), lenB = B.size();
-    if (v%2 == 0) {
-        if (A.size() & 1) {
-            A.push_back((Node){1, 0, -1}); 
-            ++lenA;
-        } 
-        solve();
-    } else {
-        if (A.size() & 1) {
-            solve();
+        one = a+c, two = a+d, three = b+c, four = b+d;
+        int64 res;
+        if (two != three) {
+            res = countDown(one, two) + countMid(two+1, three) + countUp(three+1, four);
         } else {
-            v -= 2;
-            Node a, b;
-            bool ok = false;
-            if (lenA >= 2) {
-                ok = true;
-                a = A.back(); A.pop_back();
-                b = A.back(); B.pop_back();
-                lenA -= 2;
-            }
-            A.push_back((Node){1, -1000000, -1});
-            A.push_back((Node){1, -1000000, -1});
-            v -= 3;
-            solve();
-            if (ok) {
-                if (a.p + b.p > a.p + B[pb].p) {
-                    maxx += a.p + b.p;
-                    ans.push_back(a.id);
-                    ans.push_back(b.id);
-                } else {
-                    maxx += a.p + B[pb].p;
-                    ans.push_back(a.id);
-                    ans.push_back(B[pb].id);
-                }
-            } else {
-            }
+            res = countDown(one, two) + countUp(two+1, four);
         }
+        int64 tot = (b-a+1)*(d-c+1);
+        int64 div = gcd(res, tot);
+        printf("Case #%d: ", cas);
+        cout << res/div << "/" << tot/div << endl;
     }
-
-    cout << maxx << endl;
-    for (int i = 0; i < ans.size(); ++i)
-        printf("%d ", ans[i]);
-    puts("");
     return 0;
 }
