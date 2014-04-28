5e6697e06097982eebfa3084400e49e03f9c78ff
shuangde <zengshuangde@gmail.com>
2014-04-28 22:27:49 +0800

    pat

---
+++ pat/1001.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+
+int main () {
+	
+	int a, b;
+	scanf("%d%d", &a, &b);
+	char str[100];
+	sprintf(str, "%d", a+b);
+	int len = strlen(str);
+	int i = 0;
+
+	if (str[0] == '-') { putchar(str[i++]); }
+	bool first = true;
+	for ( ; i < len; ++i) {
+		if ((len-i)%3==0 && !first) putchar(',');
+		first = false;
+		putchar(str[i]);
+	}
+	puts("");
+
+	return 0;
+}
---
+++ pat/1002.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<cstring>
+using namespace std;
+const long long mod = (1e9)+7;
+
+double a[1010], b[1010];
+
+int main(){
+    int k1, k2, maxx = 0;
+    scanf("%d", &k1);
+  for (int i = 0; i < k1; ++i) {
+        int x; double y;
+        scanf("%d%lf", &x, &y);
+        a[x] = y;
+        maxx = max(maxx, x);
+  }
+
+ scanf("%d", &k2);
+  for (int i = 0; i < k2; ++i) {
+        int x; double y;
+        scanf("%d%lf", &x, &y);
+        b[x] = y;
+        maxx = max(maxx, x);
+  }
+    int cnt = 0;
+  for (int i = 0; i <= maxx; ++i) {
+        a[i] += b[i];
+        if (a[i] != 0) ++cnt;
+  }
+    printf("%d", cnt);
+    for (int i = maxx; i >= 0; --i)
+        if (a[i]) printf(" %d %.1f", i, a[i]);
+  return 0;
+}
