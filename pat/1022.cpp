ef8c2947bbf7837ad2023073c915a7fc503f4f26
shuangde <zengshuangde@gmail.com>
2014-05-12 19:20:15 +0800

    1022 AC

--- pat/1022.cpp
+++ pat/1022.cpp
@@ -13,7 +13,7 @@ using namespace std;
 typedef pair<int, int >PII;
 typedef long long LL;
 
-map<string, set<string> >list[5];
+map<string, set<string> >query[5];
 char str[820], id[12];
 
 int main() {
@@ -26,10 +26,10 @@ int main() {
 		gets(id);
 		// title
 		gets(str);
-		list[0][str].insert(id);
+		query[0][str].insert(id);
 		// author
 		gets(str);
-		list[1][str].insert(id);
+		query[1][str].insert(id);
 		// keyword
 		char ch;
 		do {
@@ -39,22 +39,22 @@ int main() {
 				str[idx++] = ch;
 			}
 			str[idx] = '\0';
-			list[2][str].insert(id);
+			query[2][str].insert(id);
 		} while (ch != '\n');
 		// publisher
 		gets(str);
-		list[3][str].insert(id);
+		query[3][str].insert(id);
 		// year
 		gets(str);
-		list[4][str].insert(id);
+		query[4][str].insert(id);
 	}
 	scanf("%d", &m);
 	for (int i = 0; i < m; ++i) {
 		int idx; scanf("%d: ", &idx); idx--;
 		gets(str);
 		printf("%d: %s\n", idx+1, str);
-		map<string,set<string> >::iterator map_it = list[idx].find(str);
-		if (map_it == list[idx].end()) {
+		map<string,set<string> >::iterator map_it = query[idx].find(str);
+		if (map_it == query[idx].end()) {
 			puts("Not Found");
 			continue;
 		} 
